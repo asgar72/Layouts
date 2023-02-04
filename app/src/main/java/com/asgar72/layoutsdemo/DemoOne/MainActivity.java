a1bc00a1fc2ec74a9b753498b0afeb3ffe754e9e
Asgar <asgarabbas532@gmail.com>
2023-02-05 02:17:18 +0530

    Some linear layout added

--- app/src/main/java/com/asgar72/layoutsdemo/DemoOne/MainActivity.java
+++ app/src/main/java/com/asgar72/layoutsdemo/DemoOne/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.Button;
 import com.asgar72.layoutsdemo.R;
 
 public class MainActivity extends AppCompatActivity {
-Button btn1,btn2,btn3;
+    Button btn1, btn2, btn3, btn4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,24 +19,32 @@ Button btn1,btn2,btn3;
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent ob = new Intent(getApplicationContext(),LinearLayoutOne.class);
+                Intent ob = new Intent(getApplicationContext(), LinearLayoutOne.class);
                 startActivity(ob);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent ob = new Intent(getApplicationContext(),linear_layout_template.class);
+                Intent ob = new Intent(getApplicationContext(), linear_layout_template.class);
                 startActivity(ob);
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent ob = new Intent(getApplicationContext(),linear_layout_template2.class);
+                Intent ob = new Intent(getApplicationContext(), linear_layout_template2.class);
+                startActivity(ob);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob = new Intent(getApplicationContext(), linear_layout_template3.class);
                 startActivity(ob);
             }
         });
