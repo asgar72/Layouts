85e723df96d673c8c5951954b46ef9c8f8de73c3
Asgar <asgarabbas532@gmail.com>
2023-02-06 01:17:49 +0530

    Constraint_Layout add

---
+++ app/src/main/java/com/asgar72/layoutsdemo/ConstraintLayout/Constraint_temp1.java
@@ -0,0 +1,16 @@
+package com.asgar72.layoutsdemo.ConstraintLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asgar72.layoutsdemo.R;
+
+public class Constraint_temp1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_temp1);
+    }
+}
